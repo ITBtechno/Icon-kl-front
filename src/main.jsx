cac42b446cd029f07251b72d282e67d8620fbb40
MiranaMiriyeva <arzumiri20@gmail.com>
2024-07-23 16:03:12 +0400

    icon-k-l

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ import NotFound from "./components/NotFound.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-<<<<<<< Updated upstream
+
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <Routes>
@@ -36,7 +36,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </Routes>
       </BrowserRouter>
     </PersistGate>
-=======
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
@@ -52,6 +52,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
->>>>>>> Stashed changes
+
   </Provider>
 );
